34c0284538dfe02217cfb5a25ff4afe0dc538a26
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-06-07 11:28:21 -0400

    Added event listeners to the buttons to change between states

--- script.js
+++ script.js
@@ -6,10 +6,14 @@
 //         console.log(data);
 //     })
 var state = 'page1';  
-var page1El = document.querySelector('#page1')
-var page2El = document.querySelector('#page2')
-var page3El = document.querySelector('#page3')
-var page4El = document.querySelector('#page4')
+var page1El = document.querySelector('#page1');
+var page2El = document.querySelector('#page2');
+var page3El = document.querySelector('#page3');
+var page4El = document.querySelector('#page4');
+var submit = document.querySelector('#submit');
+var next1 = document.querySelector('#next1');
+var next2 = document.querySelector('#next2');
+var restart = document.querySelector('#restart');
 
 function displayPages() {
     if (state === 'page1'){
@@ -18,18 +22,21 @@ function displayPages() {
         page3El.style.display = 'none';
         page4El.style.display = 'none';
     }
+
     if (state === 'page2'){
         page1El.style.display = 'none';
         page2El.style.display = 'block';
         page3El.style.display = 'none';
         page4El.style.display = 'none';
     }
+
     if (state === 'page3'){
         page1El.style.display = 'none';
         page2El.style.display = 'none';
         page3El.style.display = 'block';
-        page4El.style.display = 'none';
+        page4El.style.display = 'none'; 
     }
+
     if (state === 'page4'){
         page1El.style.display = 'none';
         page2El.style.display = 'none';
@@ -38,3 +45,31 @@ function displayPages() {
     }
 }
 
+submit.addEventListener("click", function () {
+    state = 'page2';
+    displayPages();
+});
+
+next1.addEventListener("click", function () {
+    state = 'page3';
+    displayPages();
+});
+
+next2.addEventListener("click", function () {
+    state = 'page4';
+    displayPages();
+});
+
+restart.addEventListener("click", function () {
+    state = 'page1';
+    displayPages();
+});
+
+function init() {
+    displayPages();
+};
+
+
+
+
+init();
