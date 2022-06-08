d868c4c3c5147906de511d56ebd2c2b9f146b395
Safwan Islam <saislam@davidson.edu>
2022-06-08 18:30:40 -0500

    added interactive buttons and added #3, 2, 1 spaces for the album covers

--- script.js
+++ script.js
@@ -38,6 +38,18 @@ var cover2El = document.querySelector('#cover2');
 var cover1El = document.querySelector('#cover1');
 
 
+
+
+var song3to2 = document.querySelector ('#threeToTwo');
+var song3to1 = document.querySelector ('#threeToOne');
+var song2to1 = document.querySelector ('#twoToOne');
+var song2to3 = document.querySelector ('#twoToThree');
+var song1to2 = document.querySelector ('#oneToTwo');
+var song1to3 = document.querySelector ('#oneToThree');
+
+
+
+
 function displayPages() {
     if (state === 'page1') {
         page1El.style.display = 'block';
@@ -51,7 +63,7 @@ function displayPages() {
         page2El.style.display = 'block';
         page3El.style.display = 'none';
         page4El.style.display = 'none';
-    
+
     }
 
     if (state === 'page3') {
@@ -59,7 +71,7 @@ function displayPages() {
         page2El.style.display = 'none';
         page3El.style.display = 'block';
         page4El.style.display = 'none';
-       
+
     }
 
     if (state === 'page4') {
@@ -67,7 +79,7 @@ function displayPages() {
         page2El.style.display = 'none';
         page3El.style.display = 'none';
         page4El.style.display = 'block';
-    
+
     }
 }
 
@@ -90,39 +102,40 @@ function getAlbum() {
             artistName1El.append(artist3);
             artistName2El.append(artist2);
             artistName3El.append(artist1);
-            
-            album1El.append(album3);
-            album2El.append(album2);
-            album3El.append(album1);   
+
+            album1El.append("Album:  " + album3);
+            album2El.append("Album:  " + album2);
+            album3El.append("Album:  " + album1);
 
             var img3 = document.createElement("img");
             img3.setAttribute("src", cover3);
             img3.style.height = "300px"
             img3.style.width = "300px"
+            img3.style.borderStyle = "solid";
 
             var img2 = document.createElement("img");
             img2.setAttribute("src", cover2);
             img2.style.height = "300px"
             img2.style.width = "300px"
+            img2.style.borderStyle = "solid";
 
             var img1 = document.createElement("img");
             img1.setAttribute("src", cover1);
             img1.style.height = "300px"
             img1.style.width = "300px"
-            
+            img1.style.borderStyle = "solid";
+
             cover1El.append(img1);
             cover2El.append(img2);
             cover3El.append(img3);
-            
-            callGif(artist3,2)
-            callGif(artist2,3)
-            callGif(artist1,4)
 
-            getArtist(artist3,2);
-            getArtist(artist2,3);
-            getArtist(artist1,4);
+            callGif(artist3, 2)
+            callGif(artist2, 3)
+            callGif(artist1, 4)
 
-            
+            getArtist(artist3, 2);
+            getArtist(artist2, 3);
+            getArtist(artist1, 4);
         });
 }
 
@@ -137,7 +150,7 @@ function callGif(nameEl, pageNumber) {
             var gif = document.createElement('iframe');
             gif.setAttribute("src", "https://giphy.com/embed/" + embedId);
 
-            switch(pageNumber){
+            switch (pageNumber) {
                 case 2:
                     gif1El.append(gif);
                     break;
@@ -148,15 +161,16 @@ function callGif(nameEl, pageNumber) {
                     gif3El.append(gif);
                     break;
                 default:
-                
+
             }
 
             gif.style.padding = "10px";
+            gif.style.borderStyle = "solid";
 
         })
 }
 
-function getArtist(nameEl,pageNumber) {
+function getArtist(nameEl, pageNumber) {
     fetch('https://theaudiodb.com/api/v1/json/2/search.php?s=' + nameEl)
         .then(function (response) {
             return response.json();
@@ -167,37 +181,26 @@ function getArtist(nameEl,pageNumber) {
             var artistStart = data.artists[0].intFormedYear;
             var artistGenre = data.artists[0].strGenre;
 
-            switch(pageNumber){
+            switch (pageNumber) {
                 case 2:
-                    artistStart1El.append(artistStart);
-                    artistGenre1El.append(artistGenre);
+                    artistStart1El.append("Started in:  " + artistStart);
+                    artistGenre1El.append("Genre:  " + artistGenre);
                     artistBio1El.append(artistBio);
                     break;
                 case 3:
-                    artistStart2El.append(artistStart);
-                    artistGenre2El.append(artistGenre);
+                    artistStart2El.append("Started in:  " + artistStart);
+                    artistGenre2El.append("Genre:  " + artistGenre);
                     artistBio2El.append(artistBio);
                     break;
                 case 4:
-                    artistStart3El.append(artistStart);
-                    artistGenre3El.append(artistGenre);
+                    artistStart3El.append("Started in:  " + artistStart);
+                    artistGenre3El.append("Genre:  " + artistGenre);
                     artistBio3El.append(artistBio);
                     break;
                 default:
-                
+
             }
 
-            // artistNameEl.style.fontSize = "40px";
-            // artistNameEl.style.fontWeight = "bolder";
-            // artistNameEl.style.padding = "10px";
-            // artistName2El.style.fontSize = "40px";
-            // artistName2El.style.fontWeight = "bolder";
-            // artistName2El.style.padding = "10px";
-            // artistStartEl.style.padding = "10px";
-            // artistGenreEl.style.padding = "10px";
-            // artistStartEl.style.fontSize = "18px";
-            // artistGenreEl.style.fontSize = "18px";
-            // artistBioEl.style.padding = "10px";
         })
 }
 
@@ -237,10 +240,40 @@ button1.addEventListener("click", function () {
     displayPages();
 });
 
+song1to2.addEventListener("click", function () {
+    state = 'page3';
+    displayPages();
+}); 
+
+song1to3.addEventListener("click", function () {
+    state = 'page2';
+    displayPages();
+}); 
+
+song2to1.addEventListener("click", function () {
+    state = 'page4';
+    displayPages();
+}); 
+
+song2to3.addEventListener("click", function () {
+    state = 'page2';
+    displayPages();
+}); 
+
+song3to1.addEventListener("click", function () {
+    state = 'page4';
+    displayPages();
+}); 
+
+song3to2.addEventListener("click", function () {
+    state = 'page3';
+    displayPages();
+}); 
+
+
 
 function init() {
     dateEl.append("Today's Date: " + today);
-    dateEl.style.fontSize = "18px";
     displayPages();
     getAlbum();
 }
