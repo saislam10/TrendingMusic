f34a6664ae0db1b4366d6b2954d069cddc2e3035
Safwan Islam <saislam@davidson.edu>
2022-06-07 21:21:08 -0500

    brought in a new API fetch, adjusted and tested, commented out other fetch to test

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ var germany = document.querySelector('#germany');
 var france = document.querySelector('#france');
 var inpuEl = document.querySelector('#inputEl');
 var dateEl = document.querySelector('#date');
+var nameEl = document.querySelector('#input');
 var today = moment().format("dddd, MMMM Do, YYYY");
 
 
 function displayPages() {
     if (state === 'page1') {
-        // var today = moment().format('MMMM Do YYYY');
         page1El.style.display = 'block';
         page2El.style.display = 'none';
         page3El.style.display = 'none';
@@ -48,40 +48,52 @@ function displayPages() {
     }
 }
 
-// function getSongs() {
-//     fetch('https://pokeapi.co/api/v2/pokemon/')
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             // callGif(name);
-        
-            
-           
-//         })
-// }
-
-function callGif(nameEl) {
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=ZCIh2UkoAMZsDdlMKYVbI8ksUZ3kYD1z&q=' + nameEl + '&limit=25&offset=0&rating=g&lang=en')
+function getSongs(nameEl) {
+    fetch('https://theaudiodb.com/api/v1/json/2/search.php?s=' + nameEl.value)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            var embedId = data.data[0].id
-            var embedId1 = data.data[1].id
-            var embedId2 = data.data[2].id
-            var gif1 = document.createElement('iframe');
-            var gif2  = document.createElement('iframe');
-            var gif3 = document.createElement('iframe');
-            gif1.setAttribute("src", "https://giphy.com/embed/" + embedId)
-            gif2.setAttribute("src", "https://giphy.com/embed/" + embedId1)
-            gif3.setAttribute("src", "https://giphy.com/embed/" + embedId2)
-            page2El.append(gif1);
-            page3El.append(gif2);
-            page4El.append(gif3);
+            document.innerHTML = null;
+            var one = document.createElement('p');
+            var two = document.createElement('p');
+            var three = document.createElement('p');
+            var four = document.createElement('p');
+            var artistName = data.artists[0].strArtist;
+            var artistBio = data.artists[0].strBiographyEN;
+            var artistBday = data.artists[0].intBornYear;
+            var artistGenre = data.artists[0].strGenre;
+
+            one.append(artistName);
+            two.append(artistBday);
+            three.append(artistGenre);
+            four.append(artistBio)
+            page2El.append(one, two, three, four);
+            
         })
 }
+
+// function callGif(nameEl) {
+//     fetch('https://api.giphy.com/v1/gifs/search?api_key=ZCIh2UkoAMZsDdlMKYVbI8ksUZ3kYD1z&q=' + nameEl + '&limit=25&offset=0&rating=g&lang=en')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//             var embedId = data.data[0].id
+//             var embedId1 = data.data[1].id
+//             var embedId2 = data.data[2].id
+//             var gif1 = document.createElement('iframe');
+//             var gif2  = document.createElement('iframe');
+//             var gif3 = document.createElement('iframe');
+//             gif1.setAttribute("src", "https://giphy.com/embed/" + embedId)
+//             gif2.setAttribute("src", "https://giphy.com/embed/" + embedId1)
+//             gif3.setAttribute("src", "https://giphy.com/embed/" + embedId2)
+//             page2El.append(gif1);
+//             page3El.append(gif2);
+//             page4El.append(gif3);
+//         })
+// }
 
 
 
@@ -89,6 +101,8 @@ function callGif(nameEl) {
 submit.addEventListener("click", function () {
     state = 'page2';
     displayPages();
+    getSongs(nameEl);
+    document.innerHTML = null;
 });
 
 next1.addEventListener("click", function () {
@@ -109,9 +123,8 @@ restart.addEventListener("click", function () {
 function init() {
     dateEl.append("Today's Date: " + today)    
     displayPages();
-    callGif('bulbasaur');
+    // callGif('bulbasaur');
 
-    // getSongs();
 }
 
 init();
